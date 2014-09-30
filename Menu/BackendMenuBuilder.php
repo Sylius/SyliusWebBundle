koemeet/Sylius

Initial content bundle
Conflicts:
	composer.lock
	src/Sylius/Bundle/CoreBundle/Kernel/Kernel.php
	src/Sylius/Bundle/CoreBundle/Resources/config/app/sylius.yml
	src/Sylius/Bundle/FixturesBundle/DataFixtures/PHPCR/LoadPagesData.php

src/Sylius/Bundle/WebBundle/Menu/BackendMenuBuilder.php
@@ -157,13 +157,25 @@ class BackendMenuBuilder extends MenuBuilder
         ;
 
         $child->addChild('blocks', array(
-            'route' => 'sylius_backend_block_index',
+            'route' => 'sylius_backend_simple_block_index',
             'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-large'),
         ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.blocks', $section)));
         $child->addChild('Pages', array(
-            'route' => 'sylius_backend_page_index',
+            'route' => 'sylius_backend_static_content_index',
             'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
         ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.pages', $section)));
+        $child->addChild('Menus', array(
+            'route' => 'sylius_backend_menu_index',
+            'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
+        ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.menus', $section)));
+        $child->addChild('Routes', array(
+            'route' => 'sylius_backend_route_index',
+            'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
+        ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.routes', $section)));
+        $child->addChild('Redirects', array(
+            'route' => 'sylius_backend_redirect_route_index',
+            'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
+        ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.redirects', $section)));
     }
 
     /**

Sylius/SyliusWebBundle

Menu/BackendMenuBuilder.php
@@ -157,13 +157,28 @@ class BackendMenuBuilder extends MenuBuilder
         ;
 
         $child->addChild('blocks', array(
-            'route' => 'sylius_backend_block_index',
+            'route' => 'sylius_backend_block_overview',
             'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-large'),
         ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.blocks', $section)));
         $child->addChild('Pages', array(
-            'route' => 'sylius_backend_page_index',
+            'route' => 'sylius_backend_static_content_index',
             'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
         ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.pages', $section)));
+
+        /*
+        $child->addChild('Menus', array(
+            'route' => 'sylius_backend_menu_index',
+            'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
+        ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.menus', $section)));
+        $child->addChild('Routes', array(
+            'route' => 'sylius_backend_route_index',
+            'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
+        ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.routes', $section)));
+        $child->addChild('Redirects', array(
+            'route' => 'sylius_backend_redirect_route_index',
+            'labelAttributes' => array('icon' => 'glyphicon glyphicon-th-list'),
+        ))->setLabel($this->translate(sprintf('sylius.backend.menu.%s.redirects', $section)));
+        */
     }
 
     /**